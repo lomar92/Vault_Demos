a8c2b99107dc5243000316c63d87915bddefb2cd
lomar92 <53077361+lomar92@users.noreply.github.com>
2021-11-09 08:54:27 +0100

    Delete vagrant-demos directory

--- vagrant-demos/01_versioned-static-secrets/02_versioned-static-secrets-rollback.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#Rollback to previous or specific version
-vault kv rollback  -version=1 supersecret/admin
-vault kv get  supersecret/admin
--- vagrant-demos/README.md
+++
@@ -1,3 +0,0 @@
-The demos performed in here are performed using vagrant.
-
-Make sure you have vagrant installed to get started
